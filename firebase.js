39889528099da88480f353dd57e83b7cba898d62
nurahmedovnurshah128-oss <nurahmedovnurshah128@gmail.com>
2026-07-12 15:27:06 +0600

    Refactor Firebase configuration and exports

--- firebase.js
+++ firebase.js
@@ -1,81 +1,42 @@
-/*
-==================================
- LifeFlow Ultimate
- Firebase Configuration
-==================================
-*/
+// Firebase configuration
 
-
-// Firebase SDK
 import { initializeApp } 
 from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 
 
-import { 
-getAuth 
-}
+import { getAuth }
 from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
 
-
-import {
-getFirestore
-}
+import { getFirestore }
 from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 
 
 
-// =================================
-// ВАЖНО
-// СЮДА ВСТАВИШЬ СВОЙ FIREBASE CONFIG
-// =================================
-
-
 const firebaseConfig = {
 
-    apiKey: "ТВОЙ_API_KEY",
+apiKey: "AIzaSyDA2vJmOfiTdBlaSjo5uVw2ODHD3N0VB-Y",
 
-    authDomain: "lifeflow-3f39f.firebaseapp.com",
+authDomain: "lifeflow-3f39f.firebaseapp.com",
 
-    projectId: "lifeflow-3f39f",
+projectId: "lifeflow-3f39f",
 
-    storageBucket: "lifeflow-3f39f.firebasestorage.app",
+storageBucket: "lifeflow-3f39f.firebasestorage.app",
 
-    messagingSenderId: "525748348994",
+messagingSenderId: "525748348994",
 
-    appId: "1:525748348994:web:4a0ee957514896e6cba5db",
+appId: "1:525748348994:web:4a0ee957514896e6cba5db",
 
-    measurementId: "G-XMLT67VBKF"
+measurementId: "G-XMLT67VBKF"
 
 };
 
 
-
-
-// Инициализация
 
 const app = initializeApp(firebaseConfig);
 
 
 
-
-// Авторизация
-
-const auth = getAuth(app);
+export const auth = getAuth(app);
 
 
-
-
-// База данных
-
-const db = getFirestore(app);
-
-
-
-
-// Экспорт
-
-export {
-    app,
-    auth,
-    db
-};
+export const db = getFirestore(app);
